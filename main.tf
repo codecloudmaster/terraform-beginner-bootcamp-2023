2ed3fe4acae483591f786143e5fa9cddfbada9fe
Anatolii <itcrowd007@gmail.com>
2023-09-21 10:45:56 +0000

    #12 install terraform provider and create an S3 bucket

--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+
   }
 }
 
@@ -11,10 +17,23 @@ provider "random" {
   
 }
 
+provider "aws" {
+  # Configuration options
+}
 
+
+#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string#lower
 resource "random_string" "bucket_name" {
-  length           = 16
+  length           = 32
   special          = false
+  lower            = true
+  upper            = false
+
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+ bucket = random_string.bucket_name.result
 }
 
 
