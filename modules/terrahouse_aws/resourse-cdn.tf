d48d4d31deb804744f74eec944b18f0a1bbfb8c9
Anatolii <itcrowd007@gmail.com>
2023-10-04 16:06:21 +0000

    #33 Added ClouFront distribution and policies for S3 and refactoring into two modules

---
+++ modules/terrahouse_aws/resourse-cdn.tf
@@ -0,0 +1,66 @@
+locals {
+  s3_origin_id = "terraformBootcampOrigin"
+}
+
+
+resource "aws_cloudfront_origin_access_identity" "default" {
+  comment = "AWS cloudfront identity for S3"
+}
+
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "OAC ${var.s3_bucket_name}"
+  description                       = "OAC ${var.s3_bucket_name} policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.website_bucket.bucket_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Static website hosting for: ${var.s3_bucket_name}"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+    #price_class = "PriceClass_200"
+    
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
