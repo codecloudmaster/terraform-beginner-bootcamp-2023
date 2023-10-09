3108ef718d2692a1be1e5433d313b2b4c954bae2
Anatolii <itcrowd007@gmail.com>
2023-10-09 11:21:28 +0000

    #36 only upload index.html if a content version has change.

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -45,6 +45,9 @@ data "aws_iam_policy_document" "allow_access_from_cloudfront" {
 #    key = "error.html"
 #  }
 #}
+resource "terraform_data" "content_version" {
+  input = var.content_version
+}
 
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
@@ -56,6 +59,11 @@ resource "aws_s3_object" "index_html" {
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5(var.index_html_filepath)
+  
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 resource "aws_s3_object" "error_html" {
